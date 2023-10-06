b5eef695f69fb0f7a224f731d78a240c61d64c3a
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-10-06 21:53:00 +0100

    Update main.cpp
    Added comments

--- MCPC/2021/practice/C/main.cpp
+++ MCPC/2021/practice/C/main.cpp
@@ -8,45 +8,59 @@ using namespace std;
 typedef  vector<vector<bool>> vvb;
 typedef  vector<vector<int>> vvi;
 
+typedef vector<bool> vb;
+typedef vector<int> vi;
 
-int solve(int n , int m ,int k, int l, vvb &kittens, vvb &buttons){
-    vvi  dp(n, vector<int>(m, 0));
-    vvi  raw(n, vector<int>(m, 0));
+// This function calculates the number of paths in a matrix avoiding kittens and considering buttons.
+int solve(int n, int m, int k, int l, vvb &kittens, vvb &buttons) {
+    // Create two-dimensional arrays to store the number of paths to each cell in the matrix.
+    vvi dp(n, vi(m, 0)); // this stores the number of possible paths from cell (0, 0) to cell (i, j) with at least one pressed button
+    vvi raw(n, vi(m, 0)); // this stores the number of possible paths from cell (0, 0) to cell (i, j) without necessarily pressing a button
 
+    // the number of paths from (0, 0) to (0, 0) is 1
     raw[0][0] = 1;
 
-    for( int i = 0; i < n ; i++){
-        for( int j = 0; j < m ; j++){
+    // Loop over the matrix, starting from the top-left corner.
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
 
-            if(kittens[i][j]){
+            // If the cell contains a kitten, the number of paths to that cell is 0.
+            if (kittens[i][j]) {
                 raw[i][j] = 0;
-                dp[i][j] = 0;
                 continue;
             }
 
-            if (i != 0) raw[i][j] = (raw[i][j] + raw[i-1][j]) % MOD;
+            // Otherwise, the number of paths to the cell is the sum of the number of paths to the cell from the top
+            // and the number of paths to the cell from the left.
+            if (i != 0) raw[i][j] = (raw[i][j] + raw[i - 1][j]) % MOD;
             if (j != 0) raw[i][j] = (raw[i][j] + raw[i][j - 1]) % MOD;
 
-            // if we encounter a button, then all paths leading to this cell are valid paths
-            // also check if no buttons are in the field
-            if(buttons[i][j] || (l == 0)){
+            // If the cell contains a button, then all paths leading to this cell are valid paths
+            // Also check if no buttons are in the field (then are paths are valid)
+            if (buttons[i][j] || (l == 0)) {
                 dp[i][j] = raw[i][j];
             } else {
-                if (i != 0) dp[i][j] = (dp[i][j] + dp[i-1][j]) % MOD;
+                // If the cell does not contain a button, then the number of valid paths to the cell 
+                // is the sum of the number of valid paths to the cell from the top
+                // and the number of valid paths to the cell from the left.
+                if (i != 0) dp[i][j] = (dp[i][j] + dp[i - 1][j]) % MOD;
                 if (j != 0) dp[i][j] = (dp[i][j] + dp[i][j - 1]) % MOD;
             }
         }
     }
-    return dp[n-1][m-1];
+
+    // Return the number of paths to the bottom-right corner of the matrix.
+    return dp[n - 1][m - 1];
 }
+
 int main() {
     int t, n, m, k, l, i, j;
 
     cin >> t;
     while (t--) {
         cin >> n >> m >> k >> l;
-        vvb kittens(n, vector<bool>(m, false));
-        vvb buttons(n, vector<bool>(m, false));
+        vvb kittens(n, vb(m, false));
+        vvb buttons(n, vb(m, false));
 
         for (int _ = 0; _ < k; _++) {
             cin >> i >> j;
@@ -62,5 +76,3 @@ int main() {
 
     }
 }
-
-
